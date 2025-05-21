d425f3e37514e6bef7f86b12347a77f5c719e21a
zeus <oseisammy07@gmail.com>
2025-05-21 17:14:33 +0000

    Updated variabled 2

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -2,7 +2,7 @@ number1 = 7
 number2 = 5
 add = number1 + number2
 sub = number1 - number2
-mult = num1 * num2
+mult = number1 * number2
 
 print(f"Addition of {number1} and {number2} is {add}")
 print(f"Subtration of {number1} and {number2} is {sub}")
