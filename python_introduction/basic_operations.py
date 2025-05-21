70984d4a8a658de39e96d3cec20fc2f3963b4535
zeus <oseisammy07@gmail.com>
2025-05-21 17:09:50 +0000

    Initial commit

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,9 @@
+num1 = 7
+num2 = 5
+add = num1 + num2
+sub = num1 - num2
+mult = num1 * num2
+
+print(f"Addition of {num1} and {num2} is {add}")
+print(f"Subtration of {num1} and {num2} is {sub}")
+print(f"Multiplication of {num1} and {num2} is {mult}")
